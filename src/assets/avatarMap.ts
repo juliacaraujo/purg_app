340ba214c59b68e23be0b76a79189eabbdf67aa9
Purg <purgtrihold@gmail.com>
2026-05-03 21:11:27 -0300

    fix: registrar novos avatares (IDs 2, 3, 4, 12) no avatarMap

--- src/assets/avatarMap.ts
+++ src/assets/avatarMap.ts
@@ -4,6 +4,9 @@
 
 const avatarMap: Record<number, ReturnType<typeof require>> = {
   1:  require("../../assets/avatar/alienigena_azul_avatar.png"),
+  2:  require("../../assets/avatar/animal_capivara_avatar.png"),
+  3:  require("../../assets/avatar/animal_galo_avatar.png"),
+  4:  require("../../assets/avatar/animal_pinguin_avatar.png"),
   5:  require("../../assets/avatar/elemento_agua_fuinha_avatar.png"),
   6:  require("../../assets/avatar/elemento_ar_nuvem_avatar.png"),
   7:  require("../../assets/avatar/elemento_terra_tartaruga_avatar.png"),
@@ -11,6 +14,7 @@ const avatarMap: Record<number, ReturnType<typeof require>> = {
   9:  require("../../assets/avatar/mitologia_fenix_avatar.png"),
   10: require("../../assets/avatar/mitologia_medusa_avatar.png"),
   11: require("../../assets/avatar/purg_avatar.png"),
+  12: require("../../assets/avatar/robo_avatar.png"),
 };
 
 export default avatarMap;
